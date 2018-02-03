b7895c5f7b226d552d60ea867e9912f7f7e5f396
宇直 <516725062@qq.com>
2018-02-03 18:38:31 +0800

    bug: 重启服务器才会发送消息 待修改

--- wxapp/app.js
+++ wxapp/app.js
@@ -1,85 +1,73 @@
 //app.js
 const { log, api } = require('./utils/config.js')
+const io = require('./utils/wxapp-socket-io.js')
+// const io = require('./utils/socket-io.js')
 App({
-	onLaunch: function () {
+	onLaunch:  function () {
 		// 展示本地存储能力
 		var logs = wx.getStorageSync('logs') || []
 		logs.unshift(Date.now())
 		wx.setStorageSync('logs', logs)
+		// 连接socket
+		const socket = io.connect("ws://127.0.0.1:3555")
+		socket.emit('clientInit', 'app init')
+		this.globalData.socket = socket
+
 
 		// 登录
-		wx.login({
-			success: codeData => {
-				// 发送 res.code 到后台换取 openId, sessionKey, unionId
-				let code = {
-					code: codeData.code
-				}
-				wx.request({
-					url: api.openid,
-					data: code,
-					method: 'post',
-					success: function (openidData) {
-						log('openidData', openidData)
-						wx.getSetting({
-							success: res => {
-								log('res', res)
-								if (res.authSetting['scope.userInfo']) {
-									// 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-									wx.getUserInfo({
-										success: ress => {
-											let that = this
-											console.log('ress', ress)
-											let userInfo = ress.userInfo
-											// 可以将 res 发送给后台解码出 unionId
-											// that.globalData.userInfo = ress.userInfo
-											wx.setStorageSync('userInfo', userInfo)
-											// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-											// 所以此处加入 callback 以防止这种情况
-											if (that.userInfoReadyCallback) {
-												that.userInfoReadyCallback(ress)
-											}
-											// 保存openid
-											userInfo.openid = openidData.data.openid
-											log('new userInfo', userInfo)
-											wx.request({
-												url: api.userInfo,
-												data: userInfo,
-												method: 'post',
-												success: (r) => {
-													log('发送用户信息', r)
-												}
-											})
-										}
-									})
-								}
-							}
-						})
+		// wx.login({
+		// 	success: codeData => {
+		// 		// 发送 res.code 到后台换取 openId, sessionKey, unionId
+		// 		let code = {
+		// 			code: codeData.code
+		// 		}
+		// 		wx.request({
+		// 			url: api.openid,
+		// 			data: code,
+		// 			method: 'post',
+		// 			success: function (openidData) {
+		// 				log('openidData', openidData)
+		// 				wx.getSetting({
+		// 					success: res => {
+		// 						log('res', res)
+		// 						if (res.authSetting['scope.userInfo']) {
+		// 							// 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+		// 							wx.getUserInfo({
+		// 								success: ress => {
+		// 									let that = this
+		// 									console.log('ress', ress)
+		// 									let userInfo = ress.userInfo
+		// 									// 可以将 res 发送给后台解码出 unionId
+		// 									// that.globalData.userInfo = ress.userInfo
+		// 									wx.setStorageSync('userInfo', userInfo)
+		// 									// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+		// 									// 所以此处加入 callback 以防止这种情况
+		// 									if (that.userInfoReadyCallback) {
+		// 										that.userInfoReadyCallback(ress)
+		// 									}
+		// 									// 保存openid
+		// 									userInfo.openid = openidData.data.openid
+		// 									log('new userInfo', userInfo)
+		// 									wx.request({
+		// 										url: api.userInfo,
+		// 										data: userInfo,
+		// 										method: 'post',
+		// 										success: (r) => {
+		// 											log('发送用户信息', r)
+		// 										}
+		// 									})
+		// 								}
+		// 							})
+		// 						}
+		// 					}
+		// 				})
 
-					}
-				})
-			}
-		})
-
-		// 获取用户信息
-		// wx.getSetting({
-		//   success: res => {
-		//     if (res.authSetting['scope.userInfo']) {
-		//       // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-		//       wx.getUserInfo({
-		//         success: res => {
-		//           // 可以将 res 发送给后台解码出 unionId
-		//           this.globalData.userInfo = res.userInfo
-
-		//           // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-		//           // 所以此处加入 callback 以防止这种情况
-		//           if (this.userInfoReadyCallback) {
-		//             this.userInfoReadyCallback(res)
-		//           }
-		//         }
-		//       })
-		//     }
-		//   }
+		// 			}
+		// 		})
+		// 	}
 		// })
+
+
 	},
 	globalData: {
 		userInfo: null
--- wxapp/pages/socket/socket.js
+++ wxapp/pages/socket/socket.js
@@ -1,66 +1,67 @@
 // pages/socket/socket.js
+const app = getApp()
+const { log } = require('../../utils/util')
 Page({
+	data: {
+		questionList: [
+			// {
+			// 	id: 0,
+			// 	title: '3+5+5 = ？',
+			// 	options: [
+			// 		{
+			// 			order: 'A',
+			// 			option: '12',
+			// 			anwser: false,
+			// 		},
+			// 		{
+			// 			order: 'B',
+			// 			option: '15',
+			// 			anwser: false,
+			// 		},
+			// 		{
+			// 			order: 'C',
+			// 			option: '13',
+			// 			anwser: true,
+			// 		},
+			// 		{
+			// 			order: 'D',
+			// 			option: '14',
+			// 			anwser: false,
+			// 		},
+			// 	],
+			// },
+		],
+	},
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+	onLoad: function (options) {
+		// 开始进行连接服务器
+		this.onSocketEvent()
+	},
+	// getQuestion: function(socket, data) {
+	// 	socket.emit('question', function(res) {
+	// 		log('res', res)
+	// 		this.setData('questionList', res)
+	// 	})
+	// },
+	onSocketEvent: function () {
+		const socket = app.globalData.socket
+		const that = this
+		socket.on('login', function (msg) {
+			// 用户连接后发送第一题
+			let data = that.data.questionList
+			data.push(msg)
+			that.setData({ 'questionList': data })
+			console.log('收到的到一题', that.data.questionList)
+		})
+		socket.emit('client', '来自前端的消息')
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+		//
+		// socket.on('user left', function(msg) {
+		//  wx.showToast({
+		//    title: msg.username + ' 离开了房间',
+		//    icon: 'loading',
+		//    duration: 1000
+		//  })
+		// })
+	},
 })
