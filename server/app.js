0a4a2638eb31d0a4644411dc9e05a8dcd7ee467d
宇直 <516725062@qq.com>
2018-02-03 15:56:44 +0800

    准备添加socket.io

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+// 引入模块
 const express = require('express')
 const nunjucks = require('nunjucks')
 const bodyParser = require('body-parser')
@@ -5,39 +6,38 @@ const session = require('cookie-session')
 const fs = require('fs')
 const {log} = require('./utils/utils')
 const { appId, appSecret, secretKey } = require('./config')
-// 先初始化一个 express 实例
 const app = express()
 
+// 配置 bodyParser
 app.use(bodyParser.json());
-// 设置 bodyParser
 app.use(bodyParser.urlencoded({
 	extended: true,
 }))
 
-// 配置 nunjucks 模板, 第一个参数是模板文件的路径
-nunjucks.configure('templates', {
-	autoescape: true,
-	express: app,
-})
 // 引入路由文件
 const routeIndex = require('./routes/index')
 const wechatIndex = require('./routes/wechat')
+
 // 设置路由
 app.use('/', routeIndex)
 app.use('/wx', wechatIndex)
 
-// 套路
-const run = (port = 3000, host = '') => {
-	const server = app.listen(port, host, () => {
-		const address = server.address()
-		host = address.address
-		port = address.port
-		log(`listening server at http://${host}:${port}`)
-	})
-}
 
-if (require.main === module) {
-	const port = 3555
-	const host = '127.0.0.1'
-	run(port, host)
-}
+// 运行服务器
+const server = app.listen(port = 3555, function() {
+	const host = server.address().address
+	const port = server.address().port
+	console.log("应用实例，访问地址为 http://%s:%s", host, port)
+})
+
+// 设置 socket 监听 server
+const io = require('socket.io').listen(server)
+
+io.on('connection', function() {
+	log('新用户连接成功')
+})
+
+
+
+
+
